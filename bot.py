e3d78fe739073d18ed8844640a65f510de6142dd
slavatraderpo-bit <slavatraderpo@gmail.com>
2026-05-10 10:05:50 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,10 +10,13 @@ TOKEN = "8677937283:AAFVFqWZoZ2pZuIX9TKWl8eZbvsSUdaLeqg"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# ВСТАВЬ СЮДА FILE_ID ВИДЕО
-VIDEO_FILE_ID = "AAMCBAADGQEDFw_wagABJ_5cv9-ZP39JDvKYRAMmFY4GAALLHQADpwABUH4sjNFz6G3kAQAHbQADOwQ"
+# ID STORAGE КАНАЛА
+STORAGE_CHAT_ID = -1003924101643
 
-# Кнопка
+# ID СООБЩЕНИЯ С ВИДЕО
+VIDEO_MESSAGE_ID = 2
+
+# КНОПКА
 start_kb = InlineKeyboardMarkup()
 
 start_kb.add(
@@ -23,23 +26,26 @@ start_kb.add(
     )
 )
 
-# /start
+# START
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
 
     text = (
-        "🔥 Привет! Ты в системе.\n"
+        "🔥 Привет! Ты в системе.\n\n"
+
         "Я записал для тебя 3 коротких видео — "
-        "в них вся суть метода \n\n"
+        "в них вся суть метода faceless-блогинга "
+        "через AI-персонажей.\n\n"
 
         "▸ Видео 1: Как выбрать тему для блога\n"
         "▸ Видео 2: 2 формата контента и какие нейросети использовать\n"
         "▸ Видео 3: Как монетизировать свой блог\n\n"
 
-        "Каждое — 1.5-2 минуты. Без воды.\n"
+        "Каждое — 1.5-2 минуты. Без воды.\n\n"
+
         "Готов? Жми кнопку — открою первое 👇\n\n"
 
-        "⚠️ ВНИМАНИЕ: "
+        "⚠️ ВНИМАНИЕ:\n"
         "ЭТИ ВИДЕО БУДУТ УДАЛЕНЫ ЧЕРЕЗ 48 ЧАСОВ"
     )
 
@@ -48,15 +54,16 @@ async def start(message: types.Message):
         reply_markup=start_kb
     )
 
-# Кнопка видео
+# КНОПКА ВИДЕО
 @dp.callback_query_handler(
     lambda c: c.data == "video1"
 )
 async def send_video(callback: types.CallbackQuery):
 
-    await bot.send_document(
-        callback.from_user.id,
-        document=VIDEO_FILE_ID
+    await bot.copy_message(
+        chat_id=callback.from_user.id,
+        from_chat_id=STORAGE_CHAT_ID,
+        message_id=VIDEO_MESSAGE_ID
     )
 
     await callback.answer()
